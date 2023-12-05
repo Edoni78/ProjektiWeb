b8f2c87c1f60eeafca5c05476c9b99361e7313b0
ErionG14 <98408014+ErionG14@users.noreply.github.com>
2023-12-05 20:48:20 +0100

    Log in form and Validate form finished

--- JavaScript/loginValidate.js
+++ JavaScript/loginValidate.js
@@ -1,9 +1,7 @@
 function validateForm(){
     var name = document.getElementById('name').value;
     var surname = document.getElementById('surname').value;
-    var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    var confirmPassowrd = document.getElementById('confirmPassowrd').value;
 
     var regexName = /^[A-Z][a-z]{1,8}$/;
     if(!regexName.test(name)){
@@ -17,26 +15,9 @@ function validateForm(){
         return false;
     }
 
-    var regexEmail = /^[A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/;
-    if (!regexEmail.test(email)) {
-        alert("Please enter a valid e-mail address");
-        return false;
-    }
-
     var regexpassword = /^[A-Z][\w\S]*\d{3}$/;
     if (!regexpassword.test(password)) {
         alert("Please enter a password that has 8 characters and contains at least 3 numbers");
-        return false;
-    }
-
-    var regexConfirmPassword = /^[A-Z][\w\S]*\d{3}$/;
-    if (!regexConfirmPassword.test(confirmPassowrd)) {
-        alert("Please enter a password that has 8 characters and contains at least 3 numbers");
-        return false;
-    }
-
-    if (password !== confirmPassowrd) {
-        alert("Passwords do not match");
         return false;
     }
 
